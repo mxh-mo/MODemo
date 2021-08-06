a74f99196049273037fc16a711cd9301382efba2
mikimo <mikimo@tencent.com>
2021-08-06 11:10:32 +0800

    [tidy] string extension

--- MODemoSwift/MODemoSwift/MOScannerViewController.swift
+++ MODemoSwift/MODemoSwift/MOScannerViewController.swift
@@ -137,45 +137,5 @@ class MOScannerViewController: UIViewController {
                             alpha: 1)
         return color
     }
-    
-    func testString() {
-        let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        print(alphabet[alphabet.index(alphabet.startIndex, offsetBy: 3)])
-        print(alphabet[..<4])  // ABCD
-        print(alphabet[...4])  // ABCDE
-        print(alphabet[5..<10]) // FGHIJ
-        print(alphabet[5...10]) // FGHIJK
-        print(alphabet[11...])  // LMNOPQRSTUVWXYZ
-    }
-}
-
-extension String {
-    // 参考： https://juejin.cn/post/6856418701584007176
-    subscript(_ indexs: ClosedRange<Int>) -> String {
-        let beginIndex = index(startIndex, offsetBy: indexs.lowerBound)
-        let endIndex = index(startIndex, offsetBy: indexs.upperBound)
-        return String(self[beginIndex...endIndex])
-    }
-    
-    subscript(_ indexs: Range<Int>) -> String {
-        let beginIndex = index(startIndex, offsetBy: indexs.lowerBound)
-        let endIndex = index(startIndex, offsetBy: indexs.upperBound)
-        return String(self[beginIndex..<endIndex])
-    }
-    
-    subscript(_ indexs: PartialRangeThrough<Int>) -> String {
-        let endIndex = index(startIndex, offsetBy: indexs.upperBound)
-        return String(self[startIndex...endIndex])
-    }
-    
-    subscript(_ indexs: PartialRangeFrom<Int>) -> String {
-        let beginIndex = index(startIndex, offsetBy: indexs.lowerBound)
-        return String(self[beginIndex..<endIndex])
-    }
-    
-    subscript(_ indexs: PartialRangeUpTo<Int>) -> String {
-        let endIndex = index(startIndex, offsetBy: indexs.upperBound)
-        return String(self[startIndex..<endIndex])
-    }
 }
 
